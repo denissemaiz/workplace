ce829bb6ef7663d83d9f097ee097c022b2b22934
denissemaiz <denissemaiz@gmail.com>
2023-02-13 22:22:55 -0300

    Add funciones ReservasDAL

--- DATA ACCESS LAYER/ReservasDAL.h
+++ DATA ACCESS LAYER/ReservasDAL.h
@@ -1,14 +1,21 @@
 #ifndef RESERVASDAL_H
 #define RESERVASDAL_H
-
+#include "ReservaDTO.h"
 
 class ReservasDAL
 {
-    public:
-        void reservasDiarias();
-        void reservasQuincenales();
-        void listadoPorDptoTrabajo();
-        void listadoOcupacionPorEmpleado();
+public:
+    void reservasDiarias();
+    void reservasQuincenales();
+    void listadoPorDptoTrabajo();
+    void listadoOcupacionPorEmpleado();
+    /**/
+    ReservaDTO leer(int nroRegistro);
+    bool leerTodos(ReservaDTO registros[], int cantidad);
+    bool guardar(ReservaDTO registro);
+    bool guardar(ReservaDTO registro, int nroRegistro);
+    int getCantidad();
+    int buscar(int nroReserva);
 };
 
 #endif // RESERVASDAL_H
